2bf75774ffee8491331bd6475793303b2d550ae8
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-29 15:57:33 +0100

    [UPD] Create bikes array

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const bikes = [
+    { 
+        name: "Tarmac SL7",
+        weigth: 6.7 },
+    { 
+        name: "Emonda SLR",
+        weigth: 6.55 },
+    { 
+        name: "SuperSix EVO",
+        weigth: 6.8 },
+    { 
+        name: "Dogma F",
+        weigth: 6.9 },
+    { 
+        name: "Ultimate CFR",
+        weigth: 6.52 },
+    { 
+        name: "Teammachine SLR",
+        weigth: 6.6 }
+  ];
+  
+  console.log(bikes);
+  
